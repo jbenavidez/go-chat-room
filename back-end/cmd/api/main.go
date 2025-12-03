27dba6f60da77a45f61f172f5b5d4feb7d637935
JBenavidez <jbenavidez@Mac.lan>
2025-12-02 19:30:30 -0500

    add proto to backend

--- back-end/cmd/api/main.go
+++ back-end/cmd/api/main.go
@@ -1,23 +1,37 @@
 package main
 
 import (
+	pb "backend/proto/generated"
 	"fmt"
 	"log"
 	"net/http"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 )
 
 const port = 8080
 
-type application struct{}
+type application struct {
+	GRPCClient pb.ChatMessagesServiceClient
+}
 
 func main() {
 	fmt.Println("starting back end.......")
 	var app application
 	// listten to websocket channel
 	go ListenToWsChannel()
+	//set gRPC connection
+	conn, err := grpc.Dial("broker-service:50001", grpc.WithTransportCredentials(insecure.NewCredentials()), grpc.WithBlock())
+	if err != nil {
+		panic(err)
+	}
+	//set gRPC Client
+	client := pb.NewChatMessagesServiceClient(conn)
+	app.GRPCClient = client
 
-	log.Println("Starting application on port ", port)
-	err := http.ListenAndServe(fmt.Sprintf(":%d", port), app.routes())
+	log.Println("Starting back-end on port ", port)
+	err = http.ListenAndServe(fmt.Sprintf(":%d", port), app.routes())
 	if err != nil {
 		log.Fatal(err)
 	}
